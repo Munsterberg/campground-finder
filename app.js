e00e27811e5d94dc61a0c47935018dd1a65a6c25
Munsterberg <jetrussell93@gmail.com>
2016-02-13 23:12:21 -0500

    Added logout route, and added routes to header

--- app.js
+++ app.js
@@ -146,6 +146,11 @@ app.post('/login', passport.authenticate('local', {
   
 });
 
+app.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/campgrounds');
+});
+
 app.listen(3000, function() {
   console.log('Server is listening on port 3000');
 });
