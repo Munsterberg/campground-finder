6ec7e0830e3cac8bf7bfb045e826f54dd05ddb1a
Munsterberg <jetrussell93@gmail.com>
2016-02-16 17:34:46 -0500

    Add delete route and delete button

--- routes/comments.js
+++ routes/comments.js
@@ -69,6 +69,17 @@ router.put('/:comment_id', function(req, res) {
   });
 });
 
+// DESTROY comment route
+router.delete('/:comment_id', function(req, res) {
+  Comment.findByIdAndRemove(req.params.comment_id, function(err) {
+    if(err) {
+      res.redirect('back');
+    } else {
+      res.redirect('/campgrounds/' + req.params.id);
+    }
+  });
+});
+
 function isLoggedIn(req, res, next) {
   if(req.isAuthenticated()) {
     return next();
