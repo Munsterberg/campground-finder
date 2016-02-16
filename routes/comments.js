43a480ac63a8f21d568f8489fcd3dd593862b499
Munsterberg <jetrussell93@gmail.com>
2016-02-16 17:19:03 -0500

    Add edit routes and edit form

--- routes/comments.js
+++ routes/comments.js
@@ -44,6 +44,31 @@ router.post('/', isLoggedIn, function(req, res) {
   });
 });
 
+// EDIT comment route
+router.get('/:comment_id/edit', function(req, res) {
+  Comment.findById(req.params.comment_id, function(err, comment) {
+    if(err) {
+      res.redirect('back');
+    } else {
+      res.render('comments/edit', {
+        campground_id: req.params.id,
+        comment: comment
+      });
+    }
+  }) 
+});
+
+// PUT edit route
+router.put('/:comment_id', function(req, res) {
+  Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, comment) {
+    if(err) {
+      res.redirect('back');
+    } else {
+      res.redirect('/campgrounds/' + req.params.id);
+    }
+  });
+});
+
 function isLoggedIn(req, res, next) {
   if(req.isAuthenticated()) {
     return next();
