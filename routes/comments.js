6ad7c8bed442d1e95ecec537dda144c093b655ef
Munsterberg <jetrussell93@gmail.com>
2016-02-17 22:34:30 -0500

    Create comment authorization

--- routes/comments.js
+++ routes/comments.js
@@ -36,7 +36,6 @@ router.post('/', isLoggedIn, function(req, res) {
           campground.comments.push(comment);
           // save campground
           campground.save();
-          console.log('HERE IS THE INFO:' + comment);
           res.redirect('/campgrounds/' + campground._id);
         }
       });
@@ -45,7 +44,7 @@ router.post('/', isLoggedIn, function(req, res) {
 });
 
 // EDIT comment route
-router.get('/:comment_id/edit', function(req, res) {
+router.get('/:comment_id/edit', checkCommentOwner, function(req, res) {
   Comment.findById(req.params.comment_id, function(err, comment) {
     if(err) {
       res.redirect('back');
@@ -59,7 +58,7 @@ router.get('/:comment_id/edit', function(req, res) {
 });
 
 // PUT edit route
-router.put('/:comment_id', function(req, res) {
+router.put('/:comment_id', checkCommentOwner, function(req, res) {
   Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, comment) {
     if(err) {
       res.redirect('back');
@@ -70,7 +69,7 @@ router.put('/:comment_id', function(req, res) {
 });
 
 // DESTROY comment route
-router.delete('/:comment_id', function(req, res) {
+router.delete('/:comment_id', checkCommentOwner, function(req, res) {
   Comment.findByIdAndRemove(req.params.comment_id, function(err) {
     if(err) {
       res.redirect('back');
@@ -85,6 +84,24 @@ function isLoggedIn(req, res, next) {
     return next();
   }
   res.redirect('/login');
+}
+
+function checkCommentOwner(req, res, next) {
+  if(req.isAuthenticated()) {
+    Comment.findById(req.params.comment_id, function(err, comment) {
+      if(err) {
+        res.redirect('back');
+      } else {
+        if(comment.author.id.equals(req.user._id)) {
+          next();
+        } else {
+          res.redirect('back');
+        }
+      }
+    });
+  } else {
+    res.redirect('back');
+  }
 }
 
 module.exports = router;
